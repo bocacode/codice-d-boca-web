fa018276bac0f20b2b4066768441e2b44b08d3db
jonathans199 <jso1919@gmail.com>
2022-06-20 22:11:36 -0400

    cleaned out JS

--- scripts.js
+++ scripts.js
@@ -3,16 +3,16 @@ const menuElement = document.querySelector('#menu')
 fetch('https://codice-boca.web.app/menu')
   .then(response => response.json())
   .then(menuItems => {
-    console.log(menuItems)
-    const menuCards = menuItems.map(recipe => {
-      console.log(recipe)
-
-      return `<div class="menu-item">
+    let html = ''
+    menuItems.forEach(recipe => {
+      let htmlSegment = `<div class="menu-item">
           <h3>${recipe.title}</h3>
           <p>${recipe.description}</p>
         </div>`
+
+      html += htmlSegment
     })
-    menuElement.innerHTML = menuCards.join('')
+    menuElement.innerHTML = html
   })
 
   .catch(error => console.error(error))
